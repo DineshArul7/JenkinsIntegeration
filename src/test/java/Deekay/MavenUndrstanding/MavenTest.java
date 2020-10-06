edcd262418277b527af4b11c8f9e951573016117
DineshArul7 <dinesh.ak37@gmail.com>
2020-10-06 15:10:59 +0530

    jenkins Integeration

---
+++ src/test/java/Deekay/MavenUndrstanding/MavenTest.java
@@ -0,0 +1,12 @@
+package Deekay.MavenUndrstanding;
+
+import org.testng.annotations.Test;
+
+public class MavenTest {
+	
+	@Test
+	public void start() {
+		System.out.println("Deekay Here!!!");
+	}
+
+}
---
+++ src/test/java/Deekay/MavenUndrstanding/notincludedTest.java
@@ -0,0 +1,12 @@
+package Deekay.MavenUndrstanding;
+
+import org.testng.annotations.Test;
+
+public class notincludedTest {
+	
+	@Test
+	public void notinclude() {
+		System.out.println("testNG integeration failed");
+	}
+
+}
